8648dec72b3300e2e9e09b7355810628500d4700
ScienceFair2018 <35473773+ScienceFair2018@users.noreply.github.com>
2024-05-08 17:23:42 -0400

    wound classifier

--- app.py
+++ app.py
@@ -20,24 +20,40 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 
-MODEL_PATH = 'model_resnet.h5'
+MODEL_PATH = 'wounds_model.h5'
 # Load your trained model
 model = load_model(MODEL_PATH)
-model._make_predict_function()
+#model._make_predict_function()
 
-##For creating model
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('model_resnet.h5')
+# #For creating model
+# from keras.applications.resnet50 import ResNet50
+# model = ResNet50(weights='imagenet')
+# model.save('model_resnet.h5')
 
+import keras
 def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-    # Preprocessing the image
-    x = image.img_to_array(img)
+
+    categories = ['Background', 'Diabetic Wound', 'Normal', 'Pressure Wound', 'Surgical Wound', 'Venous Wound']
+
+    # preprocessing
+    img = keras.preprocessing.image.load_img(img_path, target_size=(64, 64))
+    x = keras.preprocessing.image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x, mode='caffe')
+    x = keras.utils.normalize(x)
+
+    # make prediction
     preds = model.predict(x)
-    return preds
+    print (categories)
+    print (preds)
+    category = categories[np.argmax(preds[0])]
+    if (category == 'Background' or 'Surgical Wound' or 'Normal') and (preds[0][np.argmax(preds)] * 100 < 80):
+        if (preds[0][1] > preds[0][5]):
+            category = 'Diabetic Wound'
+        if (preds[0][5] > preds[0][3]):
+            category = 'Venous Wound'
+        else:
+            category = 'Pressure Wound'
+    return ("Model predicts a " + category)
 
 
 @app.route('/Image_Classification', methods=['GET'])
@@ -53,18 +69,18 @@ def upload():
 
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
+            basepath, 'test', secure_filename(f.filename))
         f.save(file_path)
 
         # Make prediction
         preds = model_predict(file_path, model)
-
+        print (preds)
         # Process your result for human
         # pred_class = preds.argmax(axis=-1)
-        pred_class = decode_predictions(preds, top=1)
-        result = str(pred_class[0][0][1])
-        return result
+        #pred_class = decode_predictions(preds, top=1)
+        #result = str(pred_class[0][0][1])
+        return str(preds)
     return None
 
 if __name__ == '__main__':
-      app.run(debug=True)
+      app.run()
